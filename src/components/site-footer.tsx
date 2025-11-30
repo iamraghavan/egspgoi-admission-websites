efe08a572a069e7f6f81ff4b11bdc152362256e1
1434_Raghavan <raghavanofficials@gmail.com>
2025-11-30 14:28:30 +0000

    institutions link was break into next line dont break the institutions l

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -65,8 +65,8 @@ export function SiteFooter() {
             </div>
 
             <div className="lg:col-span-9">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                     <div className="sm:col-span-2 lg:col-span-1">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+                     <div className="md:col-span-2 lg:col-span-1">
                         <h3 className="font-semibold font-headline text-lg">Institutions</h3>
                         <ul className="mt-4 space-y-2 text-sm">
                             {institutions.map(item => (
