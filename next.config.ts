e223059cf977104225e2ecfe83d46990a91cb3f1
1434_Raghavan <raghavanofficials@gmail.com>
2025-11-30 05:14:45 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid: e

--- next.config.ts
+++ next.config.ts
@@ -31,10 +31,29 @@ const nextConfig: NextConfig = {
     ],
   },
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+
+    config.module.rules.push(
+      // Re-apply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not ?? []), /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      }
+    );
+
+    // Modify the original rule to exclude `.svg` files
+    fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
   },
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -24,7 +24,7 @@ export function SiteHeader() {
 
   return (
     <>
-      <header className="sticky top-0 z-50 w-full border-b bg-background/95">
+      <header className="sticky top-0 z-50 w-full border-b bg-background">
         <div className="container flex h-16 max-w-screen-2xl items-center">
           <div className="mr-4 flex items-center">
             <Link href="/" className="mr-6 flex items-center space-x-2">
