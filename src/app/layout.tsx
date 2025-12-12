1ce37bd10ad80ed91779ba5f514d7d7f0eca87d7
1434_Raghavan <raghavanofficials@gmail.com>
2025-12-12 16:44:39 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,6 @@ export default function RootLayout({
           fontPoppins.variable,
           fontPtSans.variable
         )}
-        suppressHydrationWarning={true}
       >
         {children}
         <Toaster />
