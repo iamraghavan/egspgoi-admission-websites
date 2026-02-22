473b05b6466460b258160cab4b9d7f82978891a8
1434_Raghavan <raghavanofficials@gmail.com>
2026-02-22 12:30:23 +0000

    <!-- Meta Pixel Code --> <script> !function(f,b,e,v,n,t,s) {if(f.fbq)ret

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -141,6 +141,24 @@ const websiteSchema = {
             __html: JSON.stringify(websiteSchema),
           }}
         />
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '660713297027148');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body
         suppressHydrationWarning
@@ -150,6 +168,11 @@ const websiteSchema = {
           fontPtSans.variable
         )}
       >
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+          src="https://www.facebook.com/tr?id=660713297027148&ev=PageView&noscript=1"
+          />
+        </noscript>
         {GA_MEASUREMENT_ID && <GoogleAnalytics />}
         {children}
         <Toaster />
