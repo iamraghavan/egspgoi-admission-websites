9f8e92594b0e5052c1cdde3a19d04d3a99873e4f
1434_Raghavan <raghavanofficials@gmail.com>
2026-05-08 02:11:52 +0000

    follow my commends to add this into our website dont remove any code in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,6 +52,16 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5DHZ5K37');`}
+        </Script>
+        {/* End Google Tag Manager */}
+        
         {/* Google tag (gtag.js) */}
         <Script
           async
@@ -114,6 +124,17 @@ export default function RootLayout({
           fontPtSans.variable
         )}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5DHZ5K37"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
           src="https://www.facebook.com/tr?id=1980598682520515&ev=PageView&noscript=1"
